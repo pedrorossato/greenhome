a70b99cdda8951da26ee61b2c6e52f63caa49804
Pedro Rossato <pedro.rossato@animati.com.br>
2024-08-26 20:51:42 -0300

    FIX Redirect cache

--- front/src/app/lib/session.ts
+++ front/src/app/lib/session.ts
@@ -26,7 +26,7 @@ export async function decrypt(
     });
     return payload;
   } catch (error) {
-    console.log('Failed to verify session');
+    console.log('Failed to verify session', error);
   }
 }
 
