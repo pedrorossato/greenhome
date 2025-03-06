608a02d2e5b49df8f199a19a7b4e6a298cccf2fc
Pedro Rossato <pedro.rossato@animati.com.br>
2025-03-06 09:42:19 -0300

    MOD Layout pagina principal

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,29 +1,23 @@
 import Image from 'next/image';
-import { FaRegBuilding, FaRegClock } from 'react-icons/fa';
-import { GiCrane } from 'react-icons/gi';
+import Link from 'next/link';
 
-import RedirectPropertiesPageButton from '@/components/button/property/redirect-properties-page-button';
-import { PropertyMapsComponent } from '@/components/maps/property/index';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card } from '@/components/ui/card';
 
-import { fetcher } from '@/services/fetcher';
-import { type Property } from '@/types/properties/property';
-import { PropertyStatus } from '@/types/properties/property-status';
 import { ArrowDown } from 'lucide-react';
 
+import Administracao from '../../public/administracao.png';
+import CompraImoveis from '../../public/compraimoveis.png';
+import Contato from '../../public/contato.jpg';
+import Edificios from '../../public/edificios.png';
 import backgroundSM from '../../public/fundoSM.png';
 import Folder2 from '../../public/mainlogogreen.png';
+import Missao from '../../public/missao.png';
+import Planejamento from '../../public/planejamento.jpg';
+import Urbanizacao from '../../public/urbanizacao.png';
+import Valores from '../../public/valores.png';
+import Visao from '../../public/visao.png';
 
 export default async function Home(): Promise<JSX.Element> {
-  const properties = await fetcher<Property[]>('/property', 'GET');
-
-  const constructionCount = properties.filter(
-    (p) => p.status === PropertyStatus.CONSTRUCTION,
-  ).length;
-  const finishedCount = properties.filter(
-    (p) => p.status === PropertyStatus.FINISHED,
-  ).length;
-
   return (
     <main>
       <section
@@ -72,111 +66,114 @@ export default async function Home(): Promise<JSX.Element> {
           </div>
         </div>
       </section>
-      <section className="w-full">
-        <div className="container grid grid-cols-1 lg:grid-cols-3 gap-4 text-justify">
-          <Card
-            style={{ backgroundColor: '#F9F9F3' }}
-            className="border-primary-blue mx-3 mx-3"
-          >
-            <CardHeader>
-              <CardTitle className="text-primary-green text-center font-[playfair] text-3xl">
-                Missão
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="text-sm leading-6">
+      <section className="w-full my-20">
+        <div className="container flex flex-row flex-wrap text-center">
+          <div className="w-full md:w-1/3 p-8 border-b-4 border-primary-green md:border-b-0 md:border-r-4">
+            <div className="flex items-center justify-center">
+              <Image height={100} src={Missao} alt={'missao'} />
+            </div>
+            <h2 className="text-center font-[playfair] text-3xl my-4">
+              Missão
+            </h2>
+            <small className="leading-6">
               Transformar sonhos em realidade por meio da construção de lares,
               espaços comerciais e empreendimentos inovadores, com excelência,
               sustentabilidade e respeito às necessidades de nossos clientes.
               Buscamos agregar valor às comunidades em que atuamos, criando
               ambientes que promovem bem-estar, segurança e desenvolvimento.
-            </CardContent>
-          </Card>
-          <Card
-            style={{ backgroundColor: '#F9F9F3' }}
-            className="border-primary-blue mx-3"
-          >
-            <CardHeader>
-              <CardTitle className="text-primary-green text-center font-[playfair] text-3xl">
-                Visão
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="text-sm leading-6">
+            </small>
+          </div>
+          <div className="w-full md:w-1/3 p-8 border-b-4 border-primary-green md:border-b-0 md:border-r-4">
+            <div className="flex items-center justify-center">
+              <Image height={100} src={Visao} alt={'missao'} />
+            </div>
+            <h2 className="text-center font-[playfair] text-3xl my-4">Visão</h2>
+            <small className="leading-6">
               Ser referência regional no setor de construção e incorporação,
               reconhecida pela qualidade de nossos projetos, compromisso com a
               sustentabilidade e satisfação de nossos clientes. Almejamos
               liderar o mercado, inovando continuamente e contribuindo para o
               crescimento urbano e social de forma responsável e eficiente.
-            </CardContent>
-          </Card>
-          <Card
-            style={{ backgroundColor: '#F9F9F3' }}
-            className="border-primary-blue mx-3"
-          >
-            <CardHeader>
-              <CardTitle className="text-primary-green text-center font-[playfair] text-3xl">
-                Valores
-              </CardTitle>
-            </CardHeader>
-            <CardContent className="text-sm leading-5">
-              <ul>
-                <li>
-                  <b>Qualidade:</b> Excelência em cada etapa, do planejamento à
-                  entrega.
-                </li>
-                <li>
-                  <b>Sustentabilidade:</b> Práticas que respeitam o meio
-                  ambiente e preservam recursos naturais.
-                </li>
-                <li>
-                  <b>Inovação:</b> Soluções criativas que agregam valor e
-                  atendem às demandas atuais e futuras.
-                </li>
-                <li>
-                  <b>Transparência:</b> Relações éticas, claras e honestas com
-                  todos os envolvidos.
-                </li>
-                <li>
-                  <b>Compromisso Social:</b> Desenvolvimento comunitário,
-                  geração de empregos e valorização da cultura local.
-                </li>
-                <li>
-                  <b>Segurança:</b> Garantia de proteção para colaboradores e
-                  futuros usuários.
-                </li>
-                <li>
-                  <b>Valorização das Pessoas:</b> Reconhecimento e incentivo ao
-                  crescimento profissional e pessoal.
-                </li>
-              </ul>
-            </CardContent>
-          </Card>
+            </small>
+          </div>
+          <div className="w-full md:w-1/3 p-8">
+            <div className="flex items-center justify-center">
+              <Image height={100} src={Valores} alt={'missao'} />
+            </div>
+            <h2 className="text-center font-[playfair] text-3xl my-4">
+              Valores
+            </h2>
+            <small className="leading-6">
+              Qualidade; Sustentabilidade; Inovação; Transparência; Compromisso
+              Social; Segurança; Valorização das Pessoas
+            </small>
+          </div>
+        </div>
+      </section>
+      <section className="w-full mt-20 bg-primary-gray">
+        <div className="container">
+          <h2 className="text-4xl font-[playfair] text-center py-5">
+            Nossos Serviços
+          </h2>
+          <div className="flex flex-wrap flex-row text-center">
+            <div className="w-full md:w-1/2 p-4 flex flex-col items-center justify-center ">
+              <Image height={80} src={CompraImoveis} alt="Serviços imoveis" />
+              <p>Compra, Venda e Loteamento de Imóveis Próprios</p>
+            </div>
+            <div className="w-full md:w-1/2 p-4 flex flex-col items-center justify-center">
+              <Image height={80} src={Administracao} alt="Serviços imoveis" />
+              <p>Administração de Obras</p>
+            </div>
+            <div className="w-full md:w-1/2 p-4 flex flex-col items-center justify-center">
+              <Image height={80} src={Urbanizacao} alt="Serviços imoveis" />
+              <p>Obras de Urbanização</p>
+            </div>
+            <div className="w-full md:w-1/2 p-4 flex flex-col items-center justify-center">
+              <Image height={80} src={Edificios} alt="Serviços imoveis" />
+              <p>Construção de Edifícios</p>
+            </div>
+          </div>
         </div>
       </section>
       <section className="w-full">
         <div className="container py-10">
-          <div className="p-5 rounded-3xl flex flex-row flex-wrap justify-center items-center">
-            <div className="w-full md:w-1/3 flex flex-col justify-center items-center">
-              <GiCrane size={50} />
-              <h1 className="text-4xl">{constructionCount}</h1>
-              empreendimento{constructionCount > 1 ? 's' : ''} em construção
-            </div>
-            <div className="w-full md:w-1/3 flex flex-col justify-center items-center">
-              <FaRegBuilding size={50} />
-              <h1 className="text-4xl">{finishedCount}</h1>
-              empreendimento{finishedCount > 1 ? 's' : ''} finalizado
-              {finishedCount > 1 ? 's' : ''}
-            </div>
-            <div className="w-full md:w-1/3 flex flex-col justify-center items-center">
-              <FaRegClock size={50} />
-              <h1 className="text-4xl">100%</h1>
-              de pontualidade na entrega
-            </div>
-          </div>
-          <div className="p-5 rounded-lg flex flex-col justify-center items-center">
-            <div className="h-72 w-3/4">
-              <PropertyMapsComponent properties={properties} />
-            </div>
-            <RedirectPropertiesPageButton className="mt-10 " />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-2">
+            <Card className="cursor-pointer overflow-hidden hover:shadow-2xl transition-all duration-300 ease-in-out">
+              <div
+                style={{
+                  backgroundImage: `url(${Planejamento.src})`,
+                  backgroundBlendMode: 'overlay',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'cover',
+                  backgroundColor: 'rgba(0, 0, 0, 0.486)',
+                }}
+              >
+                <Link
+                  className="h-80 flex items-center justify-center w-full text-2xl text-white text-center"
+                  href="/empreendimentos"
+                >
+                  <p>Visualize Nossos Empreendimentos</p>
+                </Link>
+              </div>
+            </Card>
+            <Card className="cursor-pointer overflow-hidden hover:shadow-2xl transition-all duration-300 ease-in-out">
+              <div
+                style={{
+                  backgroundImage: `url(${Contato.src})`,
+                  backgroundBlendMode: 'overlay',
+                  backgroundRepeat: 'no-repeat',
+                  backgroundSize: 'cover',
+                  backgroundColor: 'rgba(0, 0, 0, 0.486)',
+                }}
+              >
+                <Link
+                  className="h-80 flex items-center justify-center w-full text-2xl text-white text-center"
+                  href="/contato"
+                >
+                  <p>Entre em contato</p>
+                </Link>
+              </div>
+            </Card>
           </div>
         </div>
       </section>
